c7fe8f586276183e594b4bb0b574aed63ac7ba9a
Антонов <antonov.ki@phystech.edu>
2025-05-06 14:52:01 +0300

    update 18(20) lesson

---
+++ 21_1.asm
@@ -0,0 +1,37 @@
+    section .data
+    newline db 10
+
+section .bss
+    srt resb 256
+    len resq 1
+
+section .text
+    global _start
+
+_start:
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, srt
+    mov rdx, 256
+    syscall
+
+    mov [len], rax
+
+    
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, srt
+    mov rdx, [len]
+    syscall
+
+    
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, newline
+    mov rdx, 1
+    syscall
+
+    
+    mov rax, 60
+    xor rdi, rdi
+    syscall
